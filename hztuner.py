085080fdbd05e0b201321a2fa4a2ed8c2c7e6d7a
tjdwill <118497355+tjdwill@users.noreply.github.com>
2024-01-19 22:25:32 -0600

    Add files via upload
    Redesigned the program for cleanliness, style, and readability.

--- hztuner.py
+++ hztuner.py
@@ -1,114 +1,122 @@
-# -*- coding: utf-8 -*-
-
 """
-Created on Fri Mar 31 19:23:15 2023
-
-@author: Tj
-@title: Song Tuner
+@title: Frequency Tuner
+@author: tjdwill
+@date: 19 January 2024
 @description:
-A script that takes songs (assumed to be tuned to A440 Hz and converts to a solfeggio frequency.
-
-ffmpeg -i "C:/Users/user/Music/Sabrina_Claudio/No Rain, No Flowers/08 - Messages From Her.mp3" -filter:a rubberband=pitch=0.9818181818181818 -y -acodec libmp3lame -b:a 256k "C:/Users/Tj/Music/test2.mp3"
-
-Arguments explained: 
--i is the input file
--y = overwrite (no prompt)
--acodec use the 'libmp3lame' codec 
--b:a 256k audio bitrate 256 kbps
-
-Frequencies (Scale Factors):
-
-440->432 =  0.9818181818 (432/440)
-
-622.25->639 = 1.02691844114 (639/622.25)
+    Defined herein is a program that generates and executes the FFmpeg
+    command that tunes a song to the selected frequency. Full description
+    is available in the documentation.
 """
-import os
-import sys
+
+import argparse
+from pathlib import Path
+
 import ffmpy
 
-# Make the song conversion function and use throughout the script
-def songTune(songPath: str):
-    #print(songPath)
-    base, ext = os.path.splitext(songPath)
-    appending_string = ' ({}Hz){}'.format(freq, ext)
-    out_title = base + appending_string
-    #print(base,'|' ,ext)
-    #print(out_title)
-    
-    # .mp3 support
-    if ext == '.mp3':
-        ff=ffmpy.FFmpeg(
-            inputs={songPath: None},
-            outputs={os.path.splitext(songPath)[0] + appending_string: '-af "rubberband=pitch={}" -y -acodec libmp3lame -b:a 256k'.format(factor)})
-    # .opus support
-    elif ext == '.opus':
-        ff=ffmpy.FFmpeg(
-        inputs={songPath: None},
-        outputs={os.path.splitext(songPath)[0] + appending_string: '-af "rubberband=pitch={}" -y -acodec libopus -b:a 128k'.format(factor)})
-    # file not supported
+# Invariants
+SUPPORTED_FREQ = [432, 639]
+SUPPORTED_TYPES = [".mp3", ".opus", ".flac"]
+CONVERSION_FACTORS = {
+    432: (432./440.),
+    639: 639/622.25,
+}
+codec_key = "codec"
+bitrate_key = "bitrate"
+AUDIO_INFO = {
+    ".mp3": {codec_key: "libmp3lame", bitrate_key: "256k"},
+    ".opus": {codec_key: "libopus", bitrate_key: "128k"},
+    ".flac": {codec_key: "flac", bitrate_key: "48k"}
+}
+DEFAULT_FREQ = 432
+DEFAULT_EXT = ".mp3"
+
+
+# Helpers
+def is_audio(name: Path) -> bool:
+    answer = False
+    if isinstance(name, str):
+        name = Path(name)
+    if name.is_file() and name.suffix in SUPPORTED_TYPES:
+        answer = True
+    return answer
+
+
+def tune(song: Path, out_path: Path, hz: int, ext: str, dry_run: bool) -> None:
+    """Convert the song in question"""
+    # Pathing
+    name = song.stem
+    filename = out_path / f"{name}_({hz}Hz){ext}"
+    ext_info = AUDIO_INFO[ext]
+    codec, bitrate = ext_info[codec_key], ext_info[bitrate_key]
+    factor = CONVERSION_FACTORS[hz]
+
+    # Create the conversion object
+    ff = ffmpy.FFmpeg(
+        inputs={song.resolve(): None},
+        outputs={filename.resolve(): f'-af "rubberband=pitch={factor}" -y -acodec {codec} -b:a {bitrate}'}
+    )
+    if dry_run:
+        print(ff.cmd)
     else:
-        print("File type {} not supported".format(ext),'\n')
-        return -1
-        
-    #print(ff.cmd)
-    ff.run()
+        ff.run()
 
 
-# get command-line arguments and set variables
-args=sys.argv
-path = args[1]
-freq = args[2] # set scale factor later
+def tune_songs(curr_dir: Path, dir_list: list, hz: int, ext: str, dry_run: bool) -> list:
+    """Traverse directory and convert songs as encountered"""
+    next_dirs = [path for path in curr_dir.iterdir() if path.is_dir()]
+    audio_files = [track for track in curr_dir.iterdir() if is_audio(track)]
+    if audio_files:
+        converted = curr_dir / f"{hz}Hz"
+        if (not converted.is_dir()) and (not dry_run):
+            converted.mkdir()
+        for track in audio_files:
+            tune(track, converted, hz, ext, dry_run)
+    dir_list.remove(curr_dir)
+    return [*next_dirs, *path_list]
 
 
-# frequency handling
-freqs=[432, 639] # accepted frequencies
-try:
- numfreq = float(freq)
-except:
-  print("Error. Could not convert to a numbered type")
-  exit(-1)
-  
-if numfreq not in freqs:
-    raise Exception("Not an accepted frequency. Try any of the following: {}".format(freqs))
-else:
-   # calculate frequency factor
-   if numfreq==432:
-        factor = numfreq/440
-   elif numfreq==639:
-        factor = numfreq/622.25
-#print(f'Calculated Factor = {factor}')
+if __name__ == "__main__":
+    # Command Line Support
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--filetype",
+        choices=SUPPORTED_TYPES,
+        default=DEFAULT_EXT,
+        help=f"(Default: {DEFAULT_EXT}) Determines the output file type.\n"
+    )
+    parser.add_argument("filepath", help="Path to the audio file(s)")
+    parser.add_argument(
+        "--hz",
+        default=DEFAULT_FREQ,
+        type=int,
+        choices=SUPPORTED_FREQ,
+        help=f"(Default: {DEFAULT_FREQ}) Desired output tuning frequency"
+    )
+    parser.add_argument(
+        "-d", "--dryrun",
+        default=False,
+        action="store_true",
+        help="(Default: False) Performs a dry run of the program, "
+             "outputting the generated commands to the screen."
+    )
+    args = parser.parse_args()
+    desired_hz = args.hz
+    desired_ext = args.filetype
+    audio_path = Path(args.filepath)
+    test_run = args.dryrun
+    if not audio_path.exists:
+        raise ValueError("Provided path does not exist.\n")
+    if not (audio_path.is_file() or audio_path.is_dir()):
+        raise ValueError("Provided path is neither a file nor a directory.\n")
 
-
-# Single-song conversion or albums?
-single_song = os.path.splitext(path)[1]  #is the provided path a file or does it have an extension?
-if single_song != '':
-    songTune(path)
-    
-# Album conversion
-else:
-    album_paths = []
-
-    for entry in os.listdir(path):
-        dir_test= os.path.join(path, entry)
-        if os.path.isdir(dir_test):
-            album_paths.append(dir_test)
-        # apply conversion to songs in the parent directory -> can convert entire albums
-        else:
-            songTune(dir_test)
-    #print(album_paths)
-    
-# iterate over albums and songs
-    """Assume ONLY Audio files are in these directories"""
-    """Find a way to completely traverse a directory tree"""
-    for album in album_paths:
-        songs = os.listdir(album)
-        for song in songs:
-            song_path = os.path.join(album, song)
-            # only pass files into converter
-            if os.path.isfile(song_path):
-                songTune(song_path) 
-            else:
-                continue
-                
-# Completion message                
-print('\n',"Done!")
+    # Tuning Action
+    if is_audio(audio_path):
+        outdir = audio_path.parent / f"{desired_hz}Hz"
+        if (not outdir.is_dir()) and (not test_run):
+            outdir.mkdir()
+        tune(audio_path, outdir, desired_hz, desired_ext, test_run)
+    else:
+        path_list = [audio_path]
+        while path_list:
+            path_list = tune_songs(path_list[0], path_list, desired_hz, desired_ext, test_run)
+    print("Done!")
